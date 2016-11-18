708a00f5c00649f46514ae4e9e77b8b3432b56d5
Tim Evko <timevko@gmail.com>
2016-11-18 13:46:53 -0500

    still testing stuff

--- test/casper/tests.js
+++ test/casper/tests.js
@@ -1,10 +1,7 @@
-casper.test.begin('necklaces have load more', 1, (test) => {
+casper.test.begin('TEST', 1, (test) => {
     casper.start('/test/casper/index.html').then(() => {
-        const result = casper.evaluate(() => {
-            return !!document.querySelector('._JS-load-more');
-        });
-
-        test.assert(result, 'load more button exists');
+        test.assert(true, 'true');
+        casper.wait(10000);
     }).run(() => {
         test.done();
     });
